bd393fe94f5db8905dda8aa24fbbd8ee6fb12502
Adjel <adjel.touaoula@gmail.com>
2024-04-17 17:49:57 +0200

    created new NavLink Components

--- src/app/component/Navigation/Navigation.js
+++ src/app/component/Navigation/Navigation.js
@@ -1,5 +1,34 @@
+"use client";
 import React from "react";
+import styled from "styled-components";
+import NavLink from "../NavLink/NavLink";
 
-function Navigation() {}
+const links = [
+  { name: "presentation" },
+  { name: "galeries photo" },
+  { name: "avis" },
+  { name: "contact" },
+];
+
+function Navigation() {
+  return (
+    <Nav>
+      {links.map(({ name }) => (
+        <NavLink>{name}</NavLink>
+      ))}
+    </Nav>
+  );
+}
+
+const Nav = styled.nav`
+  display: flex;
+  flex: 3;
+  justify-content: space-around;
+  align-self: center;
+  align-items: center;
+  width: auto;
+  height: auto;
+  background: inherit;
+`;
 
 export default Navigation;
