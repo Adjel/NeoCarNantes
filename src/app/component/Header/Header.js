93c7f5cfcc1bf56eb81aca955fba1a64f990739f
Adjel <adjel.touaoula@gmail.com>
2024-04-17 14:10:18 +0200

    background image in page/Home /added files for the whole architecture

---
+++ src/app/component/Header/Header.js
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+import Navigation from "../Navigation/Navigation";
+import Logo from "../Logo/Logo";
+
+const Header = () => {
+  return (
+    <>
+      <Logo />
+      <Navigation />
+    </>
+  );
+};
+
+export default Header;
