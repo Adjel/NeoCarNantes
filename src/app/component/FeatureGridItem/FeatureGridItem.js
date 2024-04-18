bb9733186e3d8418083f76abde2f494b070e3dc4
Adjel <adjel.touaoula@gmail.com>
2024-04-18 18:33:00 +0200

    features and FeatureGridItem

--- src/app/component/FeatureGridItem/FeatureGridItem.js
+++ src/app/component/FeatureGridItem/FeatureGridItem.js
@@ -1,13 +1,39 @@
 import React from "react";
-import Features from "../Features/Features";
 import Decoration from "../Decoration/Decoration";
+import styled from "styled-components";
 
-export default function FeatureGridItem({ title, content }) {
+export default function FeatureGridItem({
+  title,
+  feature,
+  subFeature,
+  featureInfo,
+}) {
   return (
-    <>
+    <Wrapper>
       <FeatureTitle>{title}</FeatureTitle>
       <Decoration />
-      <FeatureContent>{content}</FeatureContent>
-    </>
+      <FeatureContent>{feature}</FeatureContent>
+      <FeatureContent>{subFeature}</FeatureContent>
+      <FeatureContent>{featureInfo}</FeatureContent>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div`
+  flex: 1;
+  display: flex;
+  min-width: 210px;
+  min-height: 400px;
+  flex-direction: column;
+  align-items: center;
+  gap: 36px;
+  background: hsla(0, 4%, 70%, 0.55);
+  padding: 8px;
+  padding-top: 36px;
+`;
+const FeatureTitle = styled.p`
+  text-align: center;
+`;
+const FeatureContent = styled.p`
+  text-align: center;
+`;
--- src/app/component/Features/Features.js
+++ src/app/component/Features/Features.js
@@ -1,18 +1,62 @@
 import React from "react";
 import styled from "styled-components";
+import FeatureGridItem from "../FeatureGridItem/FeatureGridItem";
 
-const FeaturesData = [
+const featuresData = [
   { title: "PRESTATIONS TOUTES MARQUES", feature: "Sur Devis" },
-  { title: "REPROGRAMMATION MOTEUR STAGE 1", feature: "A partir de" },
-  { title: "REPROGRAMMATION MOTEUR ETHANOL", feature: "A partir de" },
-  { title: "VITRES TEINTEES", feature: "A partir de" },
-  { title: "VIDANGE BOITE AUTOMATIQUE", feature: "A partir de" },
-  { title: "PLAQUES D'IMMATRICLUATION", feature: "Jeu de plaques + pose" },
-  { title: "REMPLACEMENT TOUS VITRAGES", feature: "Remboursement" },
-  { title: "JANTES & ACCESSOIRES", feature: "Les 4 jantes à partir de" },
-  { title: "RENOVATIONS PHARES", feature: "Les 2 à" },
-  { title: "PEINTURE ETRIERS DE FREINS", feature: "Pour les 4 : 249 euros" },
-  { title: "RACHAT DE VEHICULE", feature: "Estimation sur demande" },
+  {
+    title: "REPROGRAMMATION MOTEUR STAGE 1",
+    feature: "A partir de 399 euros",
+    subFeature: "Gagnez en puissance et en consommation",
+  },
+  {
+    title: "REPROGRAMMATION MOTEUR ETHANOL",
+    feature: "A partir de 499 euros",
+    subFeature:
+      "Gagnez en économie à la pompe & faites un geste vert pour la planète",
+  },
+  {
+    title: "VITRES TEINTEES",
+    feature: "A partir de 139 euros",
+    subFeature: "Homologuées",
+    featureInfo: "Avec notre partenaire Sunaway 20 ans d'expérience",
+  },
+  {
+    title: "VIDANGE BOITE AUTOMATIQUE",
+    feature: "A partir de 299 euros",
+    subFeature: "Evitez le remplacement !",
+  },
+  {
+    title: "PLAQUES D'IMMATRICLUATION",
+    feature: "Jeu de plaques + pose 49 euros",
+    subFeature: "Homologuées ou Personnalisables",
+  },
+  {
+    title: "REMPLACEMENT TOUS VITRAGES",
+    feature: "Remboursement de votre franchise jusqu'à 100 euros",
+  },
+  {
+    title: "JANTES & ACCESSOIRES",
+    feature: "Les 4 jantes à partir de 399 euros",
+  },
+  {
+    title: "RENOVATIONS PHARES",
+    feature: "Les 2 à 59 euros",
+    subFeature: "Rénovation des 2 phares avant",
+  },
+  {
+    title: "PEINTURE ETRIERS DE FREINS",
+    feature: "Pour les 4 : 249 euros",
+    subFeature: "Peinture haute température",
+    featureInfo: "24 coloris au choix",
+  },
+  {
+    title: "RACHAT DE VEHICULE",
+    feature: "Estimation sur demande",
+    subFeature:
+      "Avec ou sans contrôle technique Même avec un problème mécanique",
+    featureInfo: "Sans condition de reprise d'un autre véhicule",
+  },
 ];
 
 export default function Features() {
@@ -20,7 +64,16 @@ export default function Features() {
     <Wrapper>
       <Title>PRESTATIONS</Title>
       <SubTitle>Rien que pour vous !</SubTitle>
-      <Grid></Grid>
+      <Grid>
+        {featuresData.map(({ title, feature, subFeature, featureInfo }) => (
+          <FeatureGridItem
+            title={title}
+            feature={feature}
+            subFeature={subFeature}
+            featureInfo={featureInfo}
+          />
+        ))}
+      </Grid>
     </Wrapper>
   );
 }
@@ -31,7 +84,7 @@ const Wrapper = styled.div`
   align-items: center;
   width: 100%;
   padding-top: 80px;
-  background: hsla(0, 0%, 0%, 0.62);
+  background: hsla(0, 0%, 0%, 0.7);
 `;
 const Title = styled.div`
   color: white;
@@ -50,6 +103,12 @@ const SubTitle = styled.div`
   font-weight: bold;
   font-weight: 200;
 `;
+
 const Grid = styled.div`
-  display: grid;
+  display: flex;
+  width: 65%;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 15px;
+  margin: 36px;
 `;
